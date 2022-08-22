da39e9c11fd1888fe0f093fa3a800692bd5dd354
angelica <angelica.cavero@gmail.com>
2022-08-22 11:38:00 +0200

    exercise 2

---
+++ data.js
@@ -0,0 +1,15 @@
+const data = {
+    name:"Ange",
+    age:"37",
+    areYouDeveloper: true,
+    yourBirthDate:new Date("jun, 26, 1985") ,
+    favoriteBook: {
+        title:"How to Be Perfect: The Correct Answer to Every Moral Question",
+        author:"Michael Schur",
+        date: new Date("Jan 25, 2022"),
+        url:"https://www.amazon.com/dp/B098PGY72Q?ots=1&slotNum=5&imprToken=28c1626c-57a4-7605-566&ascsubtag=[]vu[p]cl06yh4xv0000p0olwt6q1dio[i]WUIaFs[u]1[t]w[r]google.com[d]D[z]m&tag=vulture-20"
+    }
+    
+}
+
+console.log(data)
