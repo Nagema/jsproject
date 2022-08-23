4bc69cae68f6169fbde821e9bdfff3b8fdc23d3d
angelica <angelica.cavero@gmail.com>
2022-08-23 18:27:09 +0200

    string methods exersice

---
+++ strMethods.js
@@ -0,0 +1,12 @@
+const nameStr = "Ange";
+const lastNameStr = "nagema";
+const studentStr = nameStr.concat(" ", lastNameStr);
+const studentTemplateString = `${nameStr} ${lastNameStr}`;
+const estudianteMayus = studentStr.toUpperCase();
+const estudianteMinus = studentStr.toLowerCase();
+const studentStrLength = studentStr.length;
+const beginsWith = studentStr.charAt();
+const endsWith = studentStr.charAt(10);
+const withoutSpaces = studentStr.replace(" ", "");
+const includesName = studentStr.includes(nameStr);
+
