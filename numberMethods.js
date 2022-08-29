d4bab5eb39eef87c747ff2669b894a15f8a7d1cb
angelica <angelica.cavero@gmail.com>
2022-08-29 10:31:25 +0200

    methods

---
+++ numberMethods.js
@@ -0,0 +1,9 @@
+const cmHeigth = 160;
+const mtsHeight = cmHeigth / 100;
+const upperHeight = Math.ceil(mtsHeight)
+console.log(Math.ceil(upperHeight));
+const kgWeight = 54.5;
+const lowerWeight = Math.floor(kgWeight)
+console.log(Math.floor(kgWeight));
+const maxValue = Number.MAX_VALUE === (Number.MAX_VALUE + 1)
+console.log(maxValue);
