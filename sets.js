ae815c7cbd51586bc32720fd470977fce2cfff38
angelica <angelica.cavero@gmail.com>
2022-08-30 13:49:01 +0200

    sets methods

---
+++ sets.js
@@ -0,0 +1,6 @@
+const familyNames = ['Marie', 'nagema', 'james', 'Lois', 'Anna']
+const setFamilyNames = new Set(familyNames)
+setFamilyNames.add('nagema')
+console.log(setFamilyNames)
+setFamilyNames.add('Javascript')
+console.log(setFamilyNames)
